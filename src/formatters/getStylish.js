437ee9dab7b557be8df2acb643837b699de5a3ab
Andrei <andrsidorchenko@gmail.com>
2024-02-11 02:48:54 +0300

    Step 7 finishing

--- src/formatters/getStylish.js
+++ src/formatters/getStylish.js
@@ -1,42 +1,40 @@
 import _ from 'lodash';
 
-const getData = (value) => {
-  if (!_.isObject(value)) {
-    return value;
+const getData = (node, depth) => {
+  const separator = '  ';
+  if (!_.isPlainObject(node)) {
+    return node;
   }
-  const keys = Object.keys(value);
-  const result = keys.map((key) => {
-    const newKeys = value[key];
-    return `${key}: ${getData(newKeys)}`;
-  });
-  return `{\n${result.join('\n')}\n}`;
+  const keys = Object.keys(node);
+  const result = keys.map((key) => `${separator.repeat(depth + 1)}${key}: ${getData(node[key], depth + 2)}`);
+  return `{\n${result.join('\n')}\n${separator.repeat(depth - 1)}}`;
 };
 
 const getStylish = (diffObject) => {
-  const separator = ' ';
+  const separator = '  ';
   const iter = (node, depth) => {
     const lines = Object.entries(node).map(([key, {
       status, value, children, value1, value2,
     }]) => {
       switch (status) {
         case 'nested':
-          return `${separator.repeat(depth)}${key}: ${iter(children, depth + 1)}`;
+          return `${separator.repeat(depth)}  ${key}: ${iter(children, depth + 2)}`;
         case 'added':
-          return `${separator.repeat(depth)}+ ${key}: ${getData(value)}`;
+          return `${separator.repeat(depth)}+ ${key}: ${getData(value, depth + 2)}`;
         case 'deleted':
-          return `${separator.repeat(depth)}- ${key}: ${getData(value)}`;
+          return `${separator.repeat(depth)}- ${key}: ${getData(value, depth + 2)}`;
         case 'changed':
           return [
-            `${separator.repeat(depth)}- ${key}: ${getData(value1)}`,
-            `${separator.repeat(depth)}+ ${key}: ${getData(value2)}`,
+            `${separator.repeat(depth)}- ${key}: ${getData(value1, depth + 2)}`,
+            `${separator.repeat(depth)}+ ${key}: ${getData(value2, depth + 2)}`,
           ].join('\n');
         case 'unchanged':
-          return `${separator.repeat(depth)}  ${key}: ${value}`;
+          return `${separator.repeat(depth)}  ${key}: ${getData(value, depth + 2)}`;
         default:
           throw new Error(`Unknown type: ${status}`);
       }
     });
-    return `{\n${lines.filter((line) => line !== '').join('\n')}\n${separator.repeat(depth - 1)}}`;
+    return `{\n${lines.join('\n')}\n${separator.repeat(depth - 1)}}`;
   };
   return iter(diffObject, 1);
 };
