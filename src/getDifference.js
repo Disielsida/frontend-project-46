ab545d4fb402cdb35304509de12325d53b012c4f
Andrei <andrsidorchenko@gmail.com>
2024-02-11 17:34:14 +0300

    final cc working with step 7

--- src/getDifference.js
+++ src/getDifference.js
@@ -11,18 +11,28 @@ const getDiff = (object1, object2) => {
   const result = keysUnion.reduce((acc, key) => {
     const value1 = object1[key];
     const value2 = object2[key];
-    let diffEntry;
 
-    if (!_.has(object2, key)) {
-      diffEntry = { value: value1, status: 'deleted' };
-    } else if (!_.has(object1, key)) {
-      diffEntry = { value: value2, status: 'added' };
-    } else if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      diffEntry = { children: getDiff(value1, value2), status: 'nested' };
+    let status;
+    if (!_.has(object1, key)) {
+      status = 'added';
+    } else if (!_.has(object2, key)) {
+      status = 'deleted';
     } else if (_.isEqual(value1, value2)) {
-      diffEntry = { value: value1, status: 'unchanged' };
+      status = 'unchanged';
+    } else if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      status = 'nested';
     } else {
-      diffEntry = { value1, value2, status: 'changed' };
+      status = 'changed';
+    }
+
+    const diffEntry = { status };
+    if (status === 'nested') {
+      diffEntry.children = getDiff(value1, value2);
+    } else if (status === 'changed') {
+      diffEntry.value1 = value1;
+      diffEntry.value2 = value2;
+    } else {
+      diffEntry.value = (status === 'added') ? value2 : value1;
     }
 
     return { ...acc, [key]: diffEntry };
