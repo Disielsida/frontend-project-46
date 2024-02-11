c8083b1aa111729009ca120f26d83c46951781c9
Andrei <andrsidorchenko@gmail.com>
2024-02-11 23:04:12 +0300

    Fixing linting problems

--- src/getDifference.js
+++ src/getDifference.js
@@ -11,18 +11,24 @@ const getDiff = (object1, object2) => {
   const result = keysUnion.reduce((acc, key) => {
     const value1 = object1[key];
     const value2 = object2[key];
-    let diffEntry;
+    const diffEntry = {};
 
     if (!_.has(object2, key)) {
-      diffEntry = { value: value1, status: 'deleted' };
+      diffEntry.value = value1;
+      diffEntry.status = 'deleted';
     } else if (!_.has(object1, key)) {
-      diffEntry = { value: value2, status: 'added' };
+      diffEntry.value = value2;
+      diffEntry.status = 'added';
     } else if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      diffEntry = { children: getDiff(value1, value2), status: 'nested' };
+      diffEntry.children = getDiff(value1, value2);
+      diffEntry.status = 'nested';
     } else if (_.isEqual(value1, value2)) {
-      diffEntry = { value: value1, status: 'unchanged' };
+      diffEntry.value = value1;
+      diffEntry.status = 'unchanged';
     } else {
-      diffEntry = { value1, value2, status: 'changed' };
+      diffEntry.value1 = value1;
+      diffEntry.value2 = value2;
+      diffEntry.status = 'changed';
     }
 
     return { ...acc, [key]: diffEntry };
